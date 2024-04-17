76929e674c9aa507148d3bc477c3f6796286ad11
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-17 22:44:47 +0100

    update for mainnavigation

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,13 +1,13 @@
-import { Outlet } from "react-router-dom";
-
+import { Outlet } from 'react-router-dom';
+import MainNavigation from '../components/MainNavigation';
 function RootLayout() {
   return (
-    <>
+    <div>
+      <MainNavigation />
       <main>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 }
-
 export default RootLayout;
