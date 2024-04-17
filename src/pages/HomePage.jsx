c7c1518c12b04ea67d35f4a5cc90cb3e0114d79a
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-17 23:58:41 +0100

    homepage style

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,19 +7,21 @@ import ButtonCustom from '../components/UI/ButtonCustom';
 
 const bookSections = ['Children books', 'Thriller books', 'Pshsycolg'];
 
-const Item = styled(Paper)(({ theme }) => ({
+const Item = styled(Paper)(() => ({
   backgroundColor: 'transparent',
-  borderColor: 'black',
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
+  border: '1px solid red',
+  boxShadow:'none',
+  padding: '10px',
+  textAlign: 'center'
 }));
 export default function HomePage() {
   return (
-    <Box sx={{ flexGrow: 1, mt: '1rem' }}>
+    <Box sx={{ flexGrow: 1, mt: '1rem', ml: '4rem', mr: '4rem' }}>
       <Grid container spacing={2}>
-        <Grid item xs={8}>
-          <ButtonCustom buttonName="Children books" />
+        <Grid item xs={6}>
+          <Item>
+            <ButtonCustom buttonName="Children books" />
+          </Item>
         </Grid>
       </Grid>
     </Box>
