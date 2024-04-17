e579e5c08806b28a1710a3e35f3cc2e09428640e
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-18 00:08:35 +0100

    right left section design

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,23 +5,45 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import ButtonCustom from '../components/UI/ButtonCustom';
 
-const bookSections = ['Children books', 'Thriller books', 'Pshsycolg'];
+const bookSectionLeft = ['Children books', 'Thriller books', 'Pshsycolg'];
+const bookSectionRight = ['Children books', 'Thriller books', 'Pshsycolg'];
 
 const Item = styled(Paper)(() => ({
   backgroundColor: 'transparent',
   border: '1px solid red',
-  boxShadow:'none',
+  boxShadow: 'none',
   padding: '10px',
-  textAlign: 'center'
+  textAlign: 'center',
 }));
 export default function HomePage() {
   return (
     <Box sx={{ flexGrow: 1, mt: '1rem', ml: '4rem', mr: '4rem' }}>
       <Grid container spacing={2}>
+        {/* ------------------------ Left ------------------------*/}
+        <Grid item xs={3}>
+          {bookSectionLeft.map((section) => (
+            <Grid item xs={12}>
+              <Item>
+                <ButtonCustom buttonName={section} />
+              </Item>
+            </Grid>
+          ))}
+        </Grid>
+        {/* ------------------------ Middle ------------------------*/}
         <Grid item xs={6}>
           <Item>
-            <ButtonCustom buttonName="Children books" />
+            <image></image>
           </Item>
+        </Grid>
+        {/* ------------------------ Right ------------------------*/}
+        <Grid item xs={3}>
+          {bookSectionRight.map((section) => (
+            <Grid item xs={12}>
+              <Item>
+                <ButtonCustom buttonName={section} />
+              </Item>
+            </Grid>
+          ))}
         </Grid>
       </Grid>
     </Box>
