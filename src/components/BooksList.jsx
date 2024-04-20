218e863ab35fe1a234ebf9a995eeba59d212f83d
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-20 01:49:37 +0100

    Updated BookList component: replaced Button with Link for 'Back to main page', adjusted container spacing and width

--- src/components/BooksList.jsx
+++ src/components/BooksList.jsx
@@ -2,6 +2,7 @@ import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 import BookCard from './BookCard';
 
 export default function BookList({ searchedBooks, header }) {
@@ -17,8 +18,8 @@ export default function BookList({ searchedBooks, header }) {
       >
         {`${header} Books`}
       </Typography>
-      <Button variant="contained">Back to main page</Button>
-      <Grid container spacing={3} columnSpacing={20} mt={3}>
+      <Button component="a" variant="contained" href='/'>Back to main page</Button>
+      <Grid container spacing={3} columnSpacing={3} mt={3} width="80vw">
         {searchedBooks.map((book) => (
           <Grid item xs={12} md={6} lg={4} xl={3} key={book.id}>
             <BookCard {...book} />
