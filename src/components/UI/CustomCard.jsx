4e37fa80c6617f667acae325426f096bb2e8cf6a
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-20 21:29:46 +0100

    Update CustomCard component with Material-UI components, add favorite icon, and update styles

--- src/components/UI/CustomCard.jsx
+++ src/components/UI/CustomCard.jsx
@@ -1,15 +1,52 @@
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import CustomCardStyle from './CustomCardStyle';
+import { Box, Typography, Grid } from '@mui/material';
 export default function CustomCard({ volumeInfo }) {
   return (
-    <CustomCardStyle coverImage={volumeInfo.imageLinks?.thumbnail}>
-      <div className="wrap animate pop">
-        <div className="overlay">
-          <div className="image-content animate slide delay-5"></div>
+    <Box sx={{ border: '1px solid gray', borderRadius: '10px' }}>
+      <Typography variant="h4" sx={{ textAlign: 'center', mt: '10px' }}>
+        {volumeInfo.authors}
+      </Typography>
+      <CustomCardStyle coverImage={volumeInfo.imageLinks?.thumbnail}>
+        <div className="wrap">
+          <div className="overlay">
+            <div className="image-content"></div>
+          </div>
+          <div className="text">
+            <p>{volumeInfo.description}</p>
+          </div>
+          <FavoriteBorderIcon
+            className="icon"
+            sx={{
+              zIndex: '1',
+              position: 'absolute',
+              right: '10px',
+              top: '10px',
+              fontSize: '2.7rem',
+              color: '#F2B6C1',
+            }}
+          />
         </div>
-        <div className="text">
-          <p>{volumeInfo.description}</p>
-        </div>
-      </div>
-    </CustomCardStyle>
+      </CustomCardStyle>
+      <Grid
+        columnSpacing={4}
+        sx={{
+          ml: '10px',
+          display: 'flex',
+          justifyContent: 'space-between',
+          mr: '10px',
+        }}
+      >
+        <Box>
+          <p>Title: {volumeInfo.title}</p>
+          <p>Publisher: {volumeInfo.publisher}</p>
+        </Box>
+        <Box>
+          <p>Published Date: {volumeInfo.publishedDate}</p>
+          <p>Page Count: {volumeInfo.pageCount}</p>
+        </Box>
+      </Grid>
+    </Box>
   );
 }
--- src/components/UI/CustomCardStyle.jsx
+++ src/components/UI/CustomCardStyle.jsx
@@ -12,7 +12,6 @@ const CustomCardStyle = styled.div`
   p {
     font-size: max(10pt, 2.5vmin);
     line-height: 1.4;
-    color: #0e390e;
     margin-bottom: 1.5rem;
   }
 
@@ -20,22 +19,23 @@ const CustomCardStyle = styled.div`
     display: flex;
     flex-wrap: nowrap;
     justify-content: space-between;
-    width: 35vmin;
-    height: 45vmin;
-    margin: 2rem auto;
-    border: 8px solid #8767ba;
+    width: 30vmin;
+    height: 40vmin;
+    margin: 10px auto;
+    border: 1px solid gray;
     transition: 0.3s ease-in-out;
+    border-radius: 10px;
     position: relative;
     overflow: hidden;
   }
   .overlay {
     position: relative;
     display: flex;
-    flex-direction:column;
+    flex-direction: column;
     width: 100%;
     height: 100%;
+    backdrop-filter: blur(10px);
     padding: 1rem 0.75rem;
-    background: #186218;
     transition: 0.4s ease-in-out;
     z-index: 1;
   }
@@ -56,104 +56,20 @@ const CustomCardStyle = styled.div`
     position: absolute;
     top: 0;
     left: 20px;
-
     height: 90%;
     padding: 3vmin 4vmin;
-    background: #f2bbd9;
+    background: #A3D9C;
     overflow-y: scroll;
+  }
+  .icon {
+    transition: 0.3s ease-in-out;
   }
 
   .wrap:hover .overlay {
     transform: translateX(-30vmin);
   }
-  /* Animations and timing delays */
-  .animate {
-    animation-duration: 0.7s;
-    animation-timing-function: cubic-bezier(0.26, 0.53, 0.74, 1.48);
-    animation-fill-mode: backwards;
+  .wrap:hover .icon {
+    transform: translateX(-30vmin);
   }
-  /* Pop In */
-  .pop {
-    animation-name: pop;
-  }
-  @keyframes pop {
-    0% {
-      opacity: 0;
-      transform: scale(0.5, 0.5);
-    }
-    100% {
-      opacity: 1;
-      transform: scale(1, 1);
-    }
-  }
-
-  /* Slide In */
-  .slide {
-    animation-name: slide;
-  }
-  @keyframes slide {
-    0% {
-      opacity: 0;
-      transform: translate(4em, 0);
-    }
-    100% {
-      opacity: 1;
-      transform: translate(0, 0);
-    }
-  }
-
-  /* Slide Left */
-  .slide-left {
-    animation-name: slide-left;
-  }
-  @keyframes slide-left {
-    0% {
-      opacity: 0;
-      transform: translate(-40px, 0);
-    }
-    100% {
-      opacity: 1;
-      transform: translate(0, 0);
-    }
-  }
-
-  .slide-up {
-    animation-name: slide-up;
-  }
-  @keyframes slide-up {
-    0% {
-      opacity: 0;
-      transform: translateY(3em);
-    }
-    100% {
-      opacity: 1;
-      transform: translateY(0);
-    }
-  }
-
-  .delay-1 {
-    animation-delay: 0.3s;
-  }
-  .delay-2 {
-    animation-delay: 0.6s;
-  }
-  .delay-3 {
-    animation-delay: 0.9s;
-  }
-  .delay-4 {
-    animation-delay: 1.2s;
-  }
-  .delay-5 {
-    animation-delay: 1.5s;
-  }
-  ${'' /* .delay-6 {
-    animation-delay: 1.8s;
-  }
-  .delay-7 {
-    animation-delay: 2.1s;
-  }
-  .delay-8 {
-    animation-delay: 2.4s;
-  } */}
 `;
 export default CustomCardStyle;
