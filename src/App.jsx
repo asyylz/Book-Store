78ef32510f4de8971d2a27761a75999907951aa3
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-25 08:48:33 +0100

    Update App.jsx: wrap AppRouter with UserProgressContextProvider and CartContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,19 @@ import { ToastContainer } from 'react-toastify';
 import AppRouter from './router/AppRouter';
 import UserProfileContextProvider from './context/UserProfileContext.jsx';
 import AuthContextProvider from './context/AuthContext.jsx';
-import CartContext from './context/CartContext.jsx';
+import { CartContextProvider } from './context/CartContext.jsx';
+import UserProgressContextProvider from './context/UserProgressContext.jsx';
 
 function App() {
   return (
     <UserProfileContextProvider>
       <AuthContextProvider>
-        <AppRouter />
-        <ToastContainer />
+        <UserProgressContextProvider>
+          <CartContextProvider>
+            <AppRouter />
+            <ToastContainer />
+          </CartContextProvider>
+        </UserProgressContextProvider>
       </AuthContextProvider>
     </UserProfileContextProvider>
   );
