bbb0c74529d6bef0df697d7e3ca525a873079360
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-27 19:26:36 +0100

    Update FavBooksPage to use local state and real-time listener for favorite books

--- src/pages/UserPages/FavBooksPage.jsx
+++ src/pages/UserPages/FavBooksPage.jsx
@@ -2,9 +2,36 @@ import { useLoaderData } from 'react-router-dom';
 import { getDatabase, ref, get, onValue } from 'firebase/database';
 import BookCard from '../../components/BookCard';
 import { Box, Grid } from '@mui/material';
+import { useState, useEffect} from 'react';
 
 export default function FavBooksPage() {
-  const { favBooks } = useLoaderData();
+  const { favBooks: initialFavBooks, user } = useLoaderData();
+  const [favBooks, setFavBooks] = useState(initialFavBooks);
+
+  useEffect(() => {
+   // const user = JSON.parse(localStorage.getItem('user')); // Assuming user data is available
+    const db = getDatabase();
+    const favBooksRef = ref(db, `users/${user.uid}/favBooks`);
+
+    // Listener for real-time updates
+    const unsubscribe = onValue(
+      favBooksRef,
+      (snapshot) => {
+        if (snapshot.exists()) {
+          const updatedBooks = snapshot.val();
+          setFavBooks(updatedBooks);
+        } else {
+          setFavBooks([]); // Handle no data scenario
+        }
+      },
+      (error) => {
+        console.error('Failed to fetch fav books:', error);
+      }
+    );
+
+    // Cleanup function to unsubscribe when the component unmounts
+    return () => unsubscribe();
+  }, []);
   return (
     <Box sx={{ mb: '2rem' }}>
       <Grid container spacing={3} columnSpacing={3} m={3} width="80vw">
@@ -42,7 +69,7 @@ export async function loaderFavBooks() {
     if (snapshot.exists()) {
       const favBooks = snapshot.val();
 
-      return { favBooks };
+      return { favBooks, user };
     } else {
       console.log('No data available');
       return {};
