c32d104873d60239f9a8ba89e9c73d3ef9c43d20
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-27 14:46:06 +0100

    * Change useState to initialize isEbook as an object with properties ebook and free * Update linkHandler to set isEbook.ebook or isEbook.free

--- src/components/UI/ButtonCustom.jsx
+++ src/components/UI/ButtonCustom.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import ButtonStyle from './ButtonStyle.jsx';
 export default function ButtonCustom({ buttonName, id, to, side, ...props }) {
-  const [isEbook, setIsEbook] = useState(false);
+  const [isEbook, setIsEbook] = useState({ ebook: false, free: false });
   if (!to) {
     return;
   }
@@ -9,7 +9,10 @@ export default function ButtonCustom({ buttonName, id, to, side, ...props }) {
   const linkHandler = (linkName) => {
     const path = linkName.split(' ');
     if (linkName.includes('E-Books')) {
-      setIsEbook(true);
+      setIsEbook((prevState) => ({ ...prevState, ebook: true }));
+    }
+    if (linkName.includes('Free')) {
+      setIsEbook((prevState) => ({ ...prevState, free: true }));
     }
     const queryLink = path[0].toLowerCase();
     return queryLink;
@@ -19,8 +22,8 @@ export default function ButtonCustom({ buttonName, id, to, side, ...props }) {
     <ButtonStyle
       id={id}
       to={
-        isEbook
-          ? '/books?q=a&filter:ebooks'
+        isEbook.ebook
+          ? `/books?q=a&filter=${isEbook.free ? 'free-' : ''}ebooks`
           : `/books?q=subject:${linkHandler(to)}`
       }
       side={side}
