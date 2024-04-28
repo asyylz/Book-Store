09c39a1dd204e7c7304f0dcc350c0dca503c3686
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-28 17:47:47 +0100

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary">
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
+      <Link color="inherit" href="https://github.com/asyylz">
         Asiye Yaliz
       </Link>{' '}
       {new Date().getFullYear()}
