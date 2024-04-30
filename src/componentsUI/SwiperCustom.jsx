e4c3accaf79b3eb4bc59d4fd8251d202ca42e7d5
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-30 11:15:50 +0100

    screen sizes

--- src/componentsUI/SwiperCustom.jsx
+++ src/componentsUI/SwiperCustom.jsx
@@ -8,23 +8,43 @@ import { Box } from '@mui/material';
 import swiperConfig from './SwiperConfig';
 import BookCard from '../components/BookCard';
 import { useUserProfileContext } from '../context/UserProfileContext';
+import { useState, useEffect } from 'react';
 
 function SwiperCustom({ books }) {
   const { favBooksUpdated } = useUserProfileContext();
-  console.log(favBooksUpdated);
   const favBookIds = favBooksUpdated?.map((book) => book.id);
+  const [spaceBetween, setSpaceBetween] = useState(); // Default space
+  useEffect(() => {
+    const handleResize = () => {
+      const currentWidth = window.innerWidth;
+      console.log(currentWidth)
+      if (currentWidth < 439 && currentWidth > 383) {
+        setSpaceBetween(110); // Smaller gap for tablets and smaller devices
+      } else if (currentWidth < 384) {
+        setSpaceBetween(100); // Even smaller gap for very small devices
+      } else {
+        setSpaceBetween(200); // Default space for larger devices
+      }
+    };
+
+    // Set initial value
+    handleResize();
+
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+
+    // Clean up function
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <Box
       sx={{
         width: '98vw',
-        //minHeight: '70vh',
-        //position: 'absolute',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontFamily: 'Oswald',
-        //border: '2px solid green',
         boxShadow:
           'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px',
         borderRadius: '10px',
@@ -33,7 +53,7 @@ function SwiperCustom({ books }) {
       <Swiper
         {...swiperConfig}
         modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween="100px"
+        spaceBetween={spaceBetween}
         //slidesPerView={5}
         navigation
         pagination={{ clickable: true }}
